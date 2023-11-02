9d1ed0530c31bc83c335109fea79dffa8dd7d3db
Alexander <alex.teel93@gmail.com>
2023-11-02 17:56:46 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+var mainElement = document.getElementById('main');
+mainElement.remove();
+var newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Alex is the champion"
+
+document.body.appendChild(newHeader);
